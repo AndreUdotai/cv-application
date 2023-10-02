50c08882480862b36cf7fdd39fdb6034ed7a1b41
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-10-02 16:57:13 +0100

    minor change

--- src/components/Cv/MainSection/MainSection.jsx
+++ src/components/Cv/MainSection/MainSection.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import RightSide from './rightSide/RightSide';
 import LeftSide from './leftSide/LeftSide'
-import '../App.css';
 
 export default function MainSection({ skills, references, education, workHistory }) {
     return (
--- src/components/Cv/PersonalDetails/PersonalInfo.jsx
+++ src/components/Cv/PersonalDetails/PersonalInfo.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
 
-import '../App.css';
-
 function Details({ emailInput, phoneInput, addressInput }) {
     return (
         <div className='details'>
